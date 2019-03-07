c38ae930d216478e75dfd0fb0c2c7227c8eaf98d
Medi Assumani <mediassumani49@gmail.com>
2019-03-06 22:54:38 -0800

    [Add] networking files

---
+++ Networking_Starter_Project/Helpers/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  ProductHunt
+//
+//  Created by Medi Assumani on 1/29/19.
+//  Copyright © 2019 Medi Assumani. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Constant{
+    
+    static let PRODUCT_HUNT_API_BASE_URL = "https://api.producthunt.com/v1/"
+    static let TOKEN = "bb5e6925f7b89c972eb39f7763f1875de85f341fe631876bc7311305607c3360"
+    static let REDIRECT_URI = "https://localhost:3000/users/auth/producthunt/callback"
+    
+}
